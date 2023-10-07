b06511e4caf219a6462fcdf28bae068fca2a52ea
mafarto- <85512432+ManuelFarto@users.noreply.github.com>
2023-10-07 22:36:27 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,17 @@
-fetch("/pru/js/data.json")
-    .then(response => response.json())
-    .then(json => {
-        locations = json["coordinates"];
-        animateSatellite();
-    });
+fetch("/pru/js/data.json").then(response => response.json()).then(json => {
+    locations = json["coordinates"];
+})
 
+var hasCompleted = false;
 var cont = 0;
-var satelliteEl = document.querySelector('#satelite');
-var currentPosition = new THREE.Vector3();
-
-function animateSatellite() {
-    if (cont >= 617) {
+setInterval(function () {
+    if (cont == 617) {
+        hasCompleted = true;
         cont = 0;
     }
-
-    var targetPosition = new THREE.Vector3(
-        locations[cont].X / 7000,
-        locations[cont].Y / 7000,
-        locations[cont].Z / 7000 - 5
-    );
-
-    // Interpola suavemente la posición actual hacia la posición de destino
-    currentPosition.lerp(targetPosition, 0.05); // Ajusta el valor para la velocidad
-
-    satelliteEl.setAttribute('position', currentPosition.toArray().join(' '));
-
-    cont++;
-
-    requestAnimationFrame(animateSatellite);
-}
-
-// Inicializa la posición inicial
-currentPosition.set(
-    locations[0].X / 7000,
-    locations[0].Y / 7000,
-    locations[0].Z / 7000 - 5
-);
-satelliteEl.setAttribute('position', currentPosition.toArray().join(' '));
+    cont = (cont + 1)%618;
+    var sceneEl = document.querySelector('a-scene');
+    
+    sceneEl.querySelector('#satelite')
+        .setAttribute('position', (locations[cont].X / 7000) + " " + (locations[cont].Y / 7000) + " " + (locations[cont].Z / 7000 - 5));
+}, 50);
