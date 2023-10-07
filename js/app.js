4f490dbd8fc382cab69dfbafa120a2553e9c7c01
Adair <adairyves@gmail.com>
2023-10-07 17:28:36 +0200

    se mueve

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+fetch("/js/data.json").then(response => response.json()).then(json => {
+    locations = json["coordinates"];
+})
+
+var hasCompleted = false;
+var cont = 0;
+setInterval(function () {
+    if (cont == 617) {
+        hasCompleted = true;
+        cont = 0;
+    }
+    cont = (cont + 1)%618;
+    var sceneEl = document.querySelector('a-scene');
+    
+    sceneEl.querySelector('#satelite')
+        .setAttribute('position', (locations[cont].X / 7000) + " " + (locations[cont].Y / 7000) + " " + (locations[cont].Z / 7000 - 5));
+}, 50);
