6022abb487e64316222517751309e42ba0160a40
Mohamed Oulguirah <moulguir@c1r3s3.42urduliz.com>
2023-10-08 05:40:15 +0200

    pasos

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 
-fetch("/pru/js/data.json").then(response => response.json()).then(json => {
+//fetch("/js/data.json").then(response => response.json()).then(json => {
+  //  locations = json["coordinates"];
+//})
+
+//actviar para githb page oo no funciona
+fetch("js/data.json").then(response => response.json()).then(json => {
     locations = json["coordinates"];
 })
 
 var hasCompleted = false;
 var cont = 0;
-var num = 7000; 
+var num = 7000;
 setInterval(function () {
-    if (cont == 617) {
+    if (cont == 617)
+	{
         hasCompleted = true;
         cont = 0;
     }
-    cont = (cont + 1)%618;
+    cont = (cont + 1)% 618;
     var sceneEl = document.querySelector('a-scene');
-    
-    sceneEl.querySelector('#satelite')
-        .setAttribute('position', (locations[cont].X / num) + " " + (locations[cont].Y / num) + " " + (locations[cont].Z / num - 5));
-}, 50);
+
+    sceneEl.querySelector('#satelite').setAttribute('position', (locations[cont].X / num) + " " + (locations[cont].Y / num) + " " + (locations[cont].Z / num -5));
+}, 80);
+
