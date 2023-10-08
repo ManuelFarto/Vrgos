0621fa7111ac02635ae79022fa63ff86b501a35c
PIPEFD <91981749+PIPEFD@users.noreply.github.com>
2023-10-08 02:28:55 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 
+
+//fetch("/js/data.json").then(response => response.json()).then(json => {
+  //  locations = json["coordinates"];
+//})
+
+//actviar para githb page oo no funciona
 fetch("/pru/js/data.json").then(response => response.json()).then(json => {
     locations = json["coordinates"];
 })
 
 var hasCompleted = false;
 var cont = 0;
-var num = 7000; 
+var num = 7000;
 setInterval(function () {
     if (cont == 617) {
         hasCompleted = true;
@@ -13,7 +19,6 @@ setInterval(function () {
     }
     cont = (cont + 1)%618;
     var sceneEl = document.querySelector('a-scene');
-    
-    sceneEl.querySelector('#satelite')
-        .setAttribute('position', (locations[cont].X / num) + " " + (locations[cont].Y / num) + " " + (locations[cont].Z / num - 5));
-}, 50);
+
+    sceneEl.querySelector('#satelite').setAttribute('position', (locations[cont].X / num) + " " + (locations[cont].Y / num) + " " + (locations[cont].Z / num -2));
+}, 15);
